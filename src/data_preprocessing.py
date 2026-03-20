1abda18c60a4993e2612a0b1c43fe93f844bacf9
Rishika Buzunoor <rishikabuzunoor@gmail.com>
2026-03-20 10:26:05 +0530

    data-preprocessing writtten

---
+++ src/data_preprocessing.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def load_data(path):
+    df=pd.read_csv(path)
+    x=df.drop("condition", axis=1)
+    y=df["condition"]
+    return x,y
+def preprocess_data(x,y):
+    scaler=StandardScaler()
+    x_scaled=scaler.fit_transform(x)
+    return train_test_split(x_scaled,y,test_size=0.2,random_state=42)
